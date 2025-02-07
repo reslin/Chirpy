8e8c224109f99eb7479775d90e836b71ed220fc3
reslin <uwe.ploss@gmail.com>
2025-02-07 22:16:26 +0100

    add fileserver

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "net/http"
 
 func main(){
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{}
 	server.Handler = mux
 	server.Addr = ":8080"
