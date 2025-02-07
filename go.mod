85f49ad5d1abfd729bd00cf7930f56cc154ac2b4
reslin <uwe.ploss@gmail.com>
2025-02-07 22:07:50 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/reslin/Chirpy
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main(){
+	mux := http.NewServeMux()
+	server := http.Server{}
+	server.Handler = mux
+	server.Addr = ":8080"
+
+	server.ListenAndServe()
+}
